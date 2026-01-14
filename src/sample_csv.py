32d9be07e3906810d127f0793019d94f4c0cda21
phan0005 <daniel.phan@etudiant.univ-reims.fr>
2026-01-14 16:47:18 +0100

    fix: sample.py loop implementation

--- src/sample_csv.py
+++ src/sample_csv.py
@@ -1,11 +1,14 @@
 import pandas as pd
 
-input_csv = "data/extrait_bdd_sirene.csv"
-output_csv = "data/sample_sirene.csv"
 n = 1000
 seed = 42
 
-df = pd.read_csv(input_csv, low_memory=False)
-sample_df = df.sample(n=n, random_state=seed)
-sample_df.to_csv(output_csv, index=False)
+files = [
+    ("data/extrait_bdd_sirene.csv", "data/sample_sirene.csv"),
+    ("data/extrait_bdd_rne.csv", "data/sample_rne.csv"),
+]
 
+for input_csv, output_csv in files:
+    df = pd.read_csv(input_csv, low_memory=False)
+    sample_df = df.sample(n=n, random_state=seed)
+    sample_df.to_csv(output_csv, index=False)
